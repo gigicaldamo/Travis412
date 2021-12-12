0580b8f12506c1a44fffac5abe4196752917f852
gigicaldamo <gigicaldamo@gigis-mbp.home>
2021-12-12 00:56:19 -0500

    Including another divide function that handles division by zero problem using a String return.

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -13,7 +13,19 @@ public class SimpleCalculator {
 		return a * b;
 	}
 	
-	public int divide(int a, int b) {
+	public int divide1(int a, int b) {
 		return a / b;
+	}
+	
+	public String divide2(int a, int b) {
+		if(b == 0) {
+			String response = "Error. Cannot divide by zero.";
+			return response;
+		}
+		else {
+			int result = a / b;
+			String answer = Integer.toString(result);
+			return answer;
+		}
 	}
 }
--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -12,7 +12,8 @@ public class SimpleCalculatorTest {
 		assertEquals(calc.add(1, 1), 2);
 		assertEquals(calc.minus(5, 3), 2);
 		assertEquals(calc.multiply(6, 5), 30);
-		assertEquals(calc.divide(40, 8), 5);
-		assertEquals(calc.divide(40, 0), 0);
+		assertEquals(calc.divide1(40, 8), 5);
+		assertEquals(calc.divide2(40, 0), "Error. Cannot divide by zero."); 
+		assertEquals(calc.divide2(250, 5), "50");
 	}
 }
